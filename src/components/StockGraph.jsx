26f84fdf2f3e159404d36c72e7eae3029627b564
feras-3 <ferasdarwish02@gmail.com>
2025-08-10 20:23:36 +0300

    working on charts

--- src/components/StockGraph.jsx
+++ src/components/StockGraph.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useEffect, useState } from 'react'
+import 'chart.js/auto'
+import { Chart } from 'react-chartjs-2'
+import { getStock } from '../services/stock'
+import { data } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 const StockGraph = () => {
-  return <div>StockGraph</div>;
-};
+  const { symbol } = useParams()
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    const getStocks = async () => {
+      const stockData = await getStock(symbol)
+      setData({
+        labels: stockData.Dates,
+        datasets: [
+          {
+            label: 'Close Price',
+            data: stockData.data,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderWidth: 1,
+            type: 'line'
+          }
+        ]
+      })
+    }
+    getStocks()
+  }, [symbol])
 
-export default StockGraph;
+  const options = {
+    responsive: true,
+    interaction: { mode: 'index', intersect: false },
+    plugins: {
+      title: { display: true, text: `${symbol} — Close & Volume` }
+    }
+  }
+  if (!data) {
+    return <div>Loading...</div>
+  } else {
+    return <Chart type="bar" data={data} options={options} />
+  }
+}
+
+export default StockGraph
---
+++ src/services/stock.js
@@ -0,0 +1,55 @@
+import { data } from 'react-router-dom'
+import Client from './api'
+
+export const getStock = async (symbol) => {
+  try {
+    const response = await Client.get(`/stocks/${symbol}`)
+
+    const arr = []
+    const dates = []
+
+    for (let i = 1; i <= 12; i++) {
+      let month = String(i).padStart(2, '0')
+      for (let j = 1; j <= 31; j++) {
+        let day = String(j).padStart(2, '0')
+        if (
+          response['Time Series (Daily)'][`2025-${month}-${day}`]?.[
+            '4. close'
+          ] !== undefined
+        ) {
+          dates.push(`2025-${month}-${day}`)
+          arr.push(
+            response['Time Series (Daily)'][`2025-${month}-${day}`]?.[
+              '4. close'
+            ]
+          )
+        }
+      }
+    }
+    return {
+      Dates: dates,
+      data: arr
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// const arr = []
+// const dates = []
+
+// for (let i = 1; i <= 12; i++) {
+//   let month = String(i).padStart(2, '0')
+//   for (let j = 1; j <= 31; j++) {
+//     let day = String(j).padStart(2, '0')
+//     if (
+//       temp['Time Series (Daily)'][`2025-${month}-${day}`]?.['4. close'] !==
+//       undefined
+//     ) {
+//       dates.push(`2025-${month}-${day}`)
+//       arr.push(
+//         temp['Time Series (Daily)'][`2025-${month}-${day}`]?.['4. close']
+//       )
+//     }
+//   }
+// }
