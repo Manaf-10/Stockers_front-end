f822a428cfb48bac95fdffc21286541024681333
alqaassass <ali.alqassas02@gmail.com>
2025-08-11 08:50:04 +0300

    added some css

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { NewPost } from '../services/NewPost'
 import { GetPosts } from '../services/GetPost'
+import './post.css'
 
 import axios from 'axios'
 
 const Posts = ({ user }) => {
   const initialState = { title: '', description: '', img: '' }
   // let navigate = useNavigate()
-  
+
   const [post, setPost] = useState(initialState)
   const [posts, setPosts] = useState([])
 
@@ -33,55 +34,56 @@ const Posts = ({ user }) => {
     const payload = await NewPost(post)
     console.log(payload)
     setPost(initialState)
-    const updatedPosts = await GetPosts() 
+    const updatedPosts = await GetPosts()
     setPosts(updatedPosts)
   }
   console.log(user)
-    if (localStorage.getItem("token")) {
+  if (localStorage.getItem('token')) {
     return (
       <>
-      <div className='container'>
-        <h1>Add A New post Listing</h1>
-        <form className="c" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name={'title'}
-            placeholder={'title'}
-            value={post.title}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name={'img'}
-            placeholder={'image'}
-            value={post.img}
-            onChange={handleChange}
-          />
-          <textarea
-            name={'description'}
-            placeholder={'description'}
-            value={post.description}
-            onChange={handleChange}
-            className="text-area"
-            cols="30"
-            rows="10"
-            autoComplete="off"
-          ></textarea>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-      <br />
-      <br />
-
-      <h1>posts:</h1>
-      {posts.map((po) => (
-        <div key={po._id}>
-          <h3>title: {po.title}</h3>
-          <p>img: {po.img}</p>
-          <p>description: {po.description}</p>
+        <div className="container">
+          <h1>Add A New post Listing</h1>
+          <form className="c" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name={'title'}
+              placeholder={'title'}
+              value={post.title}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name={'img'}
+              placeholder={'image'}
+              value={post.img}
+              onChange={handleChange}
+            />
+            <textarea
+              name={'description'}
+              placeholder={'description'}
+              value={post.description}
+              onChange={handleChange}
+              className="text-area"
+              cols="30"
+              rows="10"
+              autoComplete="off"
+            ></textarea>
+            <button type="submit">Submit</button>
+          </form>
         </div>
-      ))}
+        <br />
+        <br />
 
+        <h1>Posts:</h1>
+        <div className="posts-list">
+          {posts.map((po) => (
+            <div key={po._id} className="post-card">
+              {po.img && <img src={po.img} alt={po.title} />}
+              <h3>{po.title}</h3>
+              <p>{po.description}</p>
+            </div>
+          ))}
+        </div>
       </>
     )
   } else {
@@ -91,7 +93,6 @@ const Posts = ({ user }) => {
       </>
     )
   }
-
 }
 
 export default Posts
