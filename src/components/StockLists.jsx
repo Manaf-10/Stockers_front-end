a470d54f5820faed82bf101009a052bf4b6c324e
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-08-11 10:26:35 +0300

    fixed the link and th styling for the stock-box

--- src/components/StockLists.jsx
+++ src/components/StockLists.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import stockList from './stocksList.json';
-import { Link } from 'react-router-dom'
+import stockList from "./stocksList.json";
+import { Link } from "react-router-dom";
 
 const formatNumber = (num) => {
   if (num >= 1_000_000_000_000) {
@@ -16,55 +16,55 @@ const formatNumber = (num) => {
   }
 };
 
-
 const StockLists = () => {
   return (
-    
     <div className="stocks-list-container">
       <h2>All stocks</h2>
       {stockList.data.map((stock) => {
         const change = stock.price.changePercent;
         let changeColor = "";
-
         if (change > 0) {
-          changeColor = "#06806b"; 
+          changeColor = "#06806b";
         } else if (change < 0) {
-          changeColor = "#cc2f3c"; 
+          changeColor = "#cc2f3c";
         } else {
-          changeColor = "#707070"; 
+          changeColor = "#707070";
         }
-
         return (
-          <div className="stock-box" key={stock.symbol}>
-            <div className="stock-img">
-              <img src="google_icon.png" alt={`${stock.symbol} image`} />
-            </div>
-            <div className="stock-symbol">
+          <>
             <Link to={`/stocks/${stock.symbol}`}>
-              {stock.symbol}
-            </Link>
+              <div className="stock-box" key={stock.symbol}>
+                <div className="stock-img">
+                  <img src="google_icon.png" alt={`${stock.symbol} image`} />
+                </div>
+                <div className="stock-symbol">{stock.symbol}</div>
+                <div className="stock-price">
+                  {stock.price.current.toFixed(2)}
+                </div>
+                <div className="stock-currency">{stock.market.currency}</div>
+                <div className="stock-change" style={{ color: changeColor }}>
+                  {change}%
+                </div>
+                <div className="stock-volume">
+                  {formatNumber(stock.price.volume)}{" "}
+                  <div className="stock-currency"> {stock.market.currency}</div>
+                </div>
+                <div className="stock-market-cap">
+                  {formatNumber(stock.market.marketCap)}{" "}
+                  <div className="stock-currency"> {stock.market.currency}</div>
+                </div>
+
+                <div className="stock-pe">{stock.market.peRatio}%</div>
+                <div className="stock-exchange">
+                  <img
+                    src={`${stock.market.exchange}.png`}
+                    alt={`${stock.market.exchange}`}
+                  />
+                </div>
+                <div className="stock-sector">{stock.company.sector}</div>
               </div>
-            <div className="stock-price">
-              {stock.price.current.toFixed(2)}
-            </div>
-            <div className="stock-currency">{stock.market.currency}</div>
-            <div
-              className="stock-change"
-              style={{ color: changeColor }}
-            >
-              {change}%
-            </div>
-            <div className="stock-volume">
-              {formatNumber(stock.price.volume)} <div className="stock-currency"> {stock.market.currency}</div>
-            </div>
-            <div className="stock-market-cap">
-              {formatNumber(stock.market.marketCap)} <div className="stock-currency"> {stock.market.currency}</div>
-            </div>
-            
-          <div className="stock-pe">{stock.market.peRatio}%</div>
-          <div className="stock-exchange"><img src={`${stock.market.exchange}.png`} alt={`${stock.market.exchange}`}/></div>
-            <div className="stock-sector">{stock.company.sector}</div>
-          </div>
+            </Link>
+          </>
         );
       })}
     </div>
