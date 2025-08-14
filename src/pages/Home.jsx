9fbdd434039adf2861c274eb4625739aad8f2d25
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-08-14 09:38:11 +0300

    added homepage paragraph (this is the mvp)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,27 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-
       <div className="main-content">
         <div className="text-section">
           <h1>Home Page</h1>
-          <h2>Add a subheading</h2>
-          <p>Add a little bit of body text Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus voluptatem nostrum suscipit illo distinctio, inventore ducimus dignissimos, veniam impedit ipsam ipsum corporis minima iusto iure molestias enim non repellendus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis incidunt laboriosam vero ducimus non ratione nisi eum architecto rerum ipsum, eveniet, obcaecati libero minima magnam id, iure recusandae dolorum. Adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aperiam, voluptas fugit tempore deserunt quo, est dolor amet alias nam veritatis! Omnis, eligendi labore quam quaerat modi perferendis accusantium hic.</p>
+          <p>
+            Stockers is your go-to platform for tracking, analyzing, and staying
+            ahead in the stock market. Whether you’re a beginner exploring your
+            first investments or an experienced trader managing your portfolio,
+            we bring you real-time data, insightful charts, and market updates
+            all in one place. Stay informed, make smarter moves, and keep your
+            eyes on the market — with Stockers, you’re always in focus
+          </p>
         </div>
 
-        <img src="../readMeImgs/stonks.png" alt="noStockToday" className="image-placeholder" />
+        <img
+          src="../readMeImgs/stonks.png"
+          alt="noStockToday"
+          className="image-placeholder"
+        />
       </div>
-
     </div>
   );
 };
 
 export default Home;
-
